c27f5223a0206629c2fa4ae06e356965712f7123
Viktoria Karklin <s83774@htw-dresden.de>
2023-01-14 17:12:34 +0100

    schluss

--- SE3/app2/views.py
+++ SE3/app2/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from app2.models import tablet
 # Create your views here.
 
 def members(request):
@@ -12,5 +13,17 @@ def projekt(request):
 def projektansicht(request):
   return render(request,'app2/templates/projektansicht.html')  
 
+
 def projekterstellen(request):
-  return render(request,'app2/templates/projekt-erstellen.html')
+  if request.method == "GET":
+    return render(request,'app2/templates/projekt-erstellen.html')
+  if request.method == "POST":
+    ID = request.POST.get('id')
+    Titel = request.POST.get('Titel')
+    Beschreibung = request.POST.get('Beschreibung')
+    Ansprechpartner = request.POST.get('Ansprechpartner')
+
+    new_tablet = tablet(ID,Titel,Beschreibung,Ansprechpartner )
+    new_tablet.save()
+
+    return render(request,'app2/templates/projektansicht.html')
