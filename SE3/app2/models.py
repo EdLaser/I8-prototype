04634e0606d3e39e98f1b9da59b75b2c58ba2168
Ruben Kraus <s80350@htw-dresden.de>
2023-01-15 13:36:57 +0100

    added autofield

--- SE3/app2/models.py
+++ SE3/app2/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-  id = models.AutoField(primary_key=True)
+  id = models.AutoField(primary_key=True, default=0)
   titel = models.CharField(max_length=255)
   beschreibung = models.CharField(max_length=255)
   ansprechpartner = models.CharField(max_length=255)
