fe56002cb0fa7a7c12f2da4b9946eee1e6c0e49f
Ruben Kraus <s80350@htw-dresden.de>
2023-01-15 13:01:01 +0100

    reworked classs

--- SE3/app2/models.py
+++ SE3/app2/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class tablet(models.Model):
-  idd = models.CharField(max_length=255)
-  Titel = models.CharField(max_length=255)
-  beschreibnung = models.CharField(max_length=255)
-  Ansprechpartner = models.CharField(max_length=255)
+class Project(models.Model):
+  id = models.AutoField(primary_key=True)
+  titel = models.CharField(max_length=255)
+  beschreibung = models.CharField(max_length=255)
+  ansprechpartner = models.CharField(max_length=255)
