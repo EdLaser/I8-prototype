5800d408c5c61cf79f418c30a72aa28a6a1ced83
Viktoria Karklin <s83774@htw-dresden.de>
2023-01-15 15:11:11 +0100

    oh man

--- SE3/app2/models.py
+++ SE3/app2/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-  id = models.AutoField(primary_key=True, default=0)
+  id = models.CharField(max_length = 255,primary_key=True)
   titel = models.CharField(max_length=255)
   beschreibung = models.CharField(max_length=255)
   ansprechpartner = models.CharField(max_length=255)
