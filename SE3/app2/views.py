238dd618dedec3ca0e269de44a6faec7da6d1848
Ruben Kraus <s80350@htw-dresden.de>
2023-01-15 13:54:06 +0100

    dynamic routing and porject detail view

--- SE3/app2/views.py
+++ SE3/app2/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from app2.models import Project
 # Create your views here.
 
@@ -9,22 +10,25 @@ def members(request):
   return render(request,'app2/templates/index.html')
 
 def projekt(request):
-  return render(request,'app2/templates/projekt.html')
+  context = {'projects' : Project.objects.all()}
+  return render(request,'app2/templates/projekt.html', context=context)
 
 def projektansicht(request, given_id):
-  wanted_project = Project.objects.filter(id=given_id)
-  return render(request,'app2/templates/projektansicht.html', project = wanted_project)  
-
+  all = Project.objects.all()
+  obj = get_object_or_404(Project, id=given_id)
+  context = {'project': obj, 'projects': all}
+  return render(request,'app2/templates/projektansicht.html', context=context)  
 
 def projekterstellen(request):
   if request.method == "GET":
     return render(request,'app2/templates/projekt-erstellen.html')
+  
   if request.method == "POST":
     titel = request.POST.get('titel')
-    beschreibung = request.POST.get('Bbeschreibung')
+    beschreibung = request.POST.get('beschreibung')
     ansprechpartner = request.POST.get('ansprechpartner')
 
-    new_project = Project(titel,beschreibung,ansprechpartner )
+    new_project = Project(titel=titel, beschreibung=beschreibung, ansprechpartner=ansprechpartner)
     new_project.save()
 
     return redirect('projektansicht', given_id = new_project.id)
