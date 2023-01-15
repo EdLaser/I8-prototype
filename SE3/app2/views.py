7664643acafa619dabe1a512c8b0abf26d85b86c
Ruben Kraus <s80350@htw-dresden.de>
2023-01-15 13:00:37 +0100

    added vieew for creazing and viewing projects

--- SE3/app2/views.py
+++ SE3/app2/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from app2.models import tablet
+from django.shortcuts import redirect
+from app2.models import Project
 # Create your views here.
 
 def members(request):
@@ -10,20 +11,20 @@ def members(request):
 def projekt(request):
   return render(request,'app2/templates/projekt.html')
 
-def projektansicht(request):
-  return render(request,'app2/templates/projektansicht.html')  
+def projektansicht(request, given_id):
+  wanted_project = Project.objects.filter(id=given_id)
+  return render(request,'app2/templates/projektansicht.html', project = wanted_project)  
 
 
 def projekterstellen(request):
   if request.method == "GET":
     return render(request,'app2/templates/projekt-erstellen.html')
   if request.method == "POST":
-    ID = request.POST.get('id')
-    Titel = request.POST.get('Titel')
-    Beschreibung = request.POST.get('Beschreibung')
-    Ansprechpartner = request.POST.get('Ansprechpartner')
+    titel = request.POST.get('titel')
+    beschreibung = request.POST.get('Bbeschreibung')
+    ansprechpartner = request.POST.get('ansprechpartner')
 
-    new_tablet = tablet(ID,Titel,Beschreibung,Ansprechpartner )
-    new_tablet.save()
+    new_project = Project(titel,beschreibung,ansprechpartner )
+    new_project.save()
 
-    return render(request,'app2/templates/projektansicht.html')
+    return redirect('projektansicht', given_id = new_project.id)
